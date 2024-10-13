2bc839cf543e11918dc095a5cb969c08a863f9e0
karan kumar <161189412+Meet-Karan@users.noreply.github.com>
2024-10-14 00:23:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+//alert("Welcome to our website")
+
+//alert("Error! Please enter a valid  password")
+ 
+//alert("Welcomr to JS land...\nHappy Coding!")
+
+//alert("Welcome to JS land...")
+//alert("Happy Coding!")
+
+//alert("Hello... I can run JS through my web browser's console");
+
